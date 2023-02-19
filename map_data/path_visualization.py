9c7ac180c0c75b7789594bc7f7f1fc7bec4b7640
Sebastian vom Dorff <github@vom-dorff.de>
2023-02-19 19:16:26 +0100

    Cleaned up naths

--- map_data/path_visualization.py
+++ map_data/path_visualization.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-path1 = np.loadtxt('map_data/garage_map_path1_2_csv.csv', delimiter=';')
-print(path1)
+paths = np.loadtxt('map_data/garage_map_paths_csv.csv', delimiter=';')
+print(paths)
 
-plt.imshow(path1)
+plt.imshow(paths)
 plt.show()
